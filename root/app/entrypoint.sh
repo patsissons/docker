3eea554a64c3f61003750082f6435167ef926a2d
Pat Sissons <patricksissons@gmail.com>
2017-01-31 00:47:43 -0800

    adding entry point script this will setup the authorized keys and forced command, and start the irssi within a screen session.

---
+++ root/app/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+SSH_CONF=$HOME/.ssh
+KEYS_FILE=$SSH_CONF/authorized_keys
+SCREEN_NAME=irc
+KEY_PREFIX=command="screen -rD $SCREEN_NAME",no-port-forwarding,no-X11-forwarding,no-pty
+
+if [ -n "$AUTHORIZED_KEYS" ]; then
+  mkdir -p $SSH_CONF
+  chmod 700 $SSH_CONF
+  touch $KEYS_FILE
+  chmod 600 $KEYS_FILE
+
+  for key in $(echo ${AUTHORIZED_KEYS} | tr "," "\n"); do
+    key=$(echo $key | sed -e 's/^ *//' -e 's/ *$//')
+    echo "=> Adding public key to .ssh/authorized_keys: $key"
+    echo "$KEY_PREFIX $key" >> $KEYS_FILE
+  done
+else
+  echo "ERROR: No AUTHORIZED_KEYS specified"
+  exit 1
+fi
+
+screen -S $SCREEN_NAME -d -m irssi
+
+exec "$@"
